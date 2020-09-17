eb4a9c7de5b62ec87b5ea0045f4def56aebef8d2
Lusaca <kny70dr@gmail.com>
2020-09-17 21:48:52 +0900

    bugs fix

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,7 @@
 class HomeController < ApplicationController
   def index
+    @posts = Post.last(4).reverse
+    @tags = Tag.last(10).reverse
   end
 
   def show
--- app/javascript/packs/main.js
+++ app/javascript/packs/main.js
@@ -6,13 +6,26 @@ document.getElementById("menu-button").addEventListener("click", function () {
         document.querySelector("nav").style.display = "block";
         this.innerHTML = '<ion-icon name="close-outline"></ion-icon>'
     };
-
+});
+document.querySelector(".find_posts_area input").addEventListener("focus", function () {
+    if (window.innerWidth <= 1060) {
+        document.querySelector(".find_posts_area").setAttribute("class", "find_posts_area find_posts_area_focus"); document.getElementById("menu-button")
+        document.getElementById("menu-button").style.display = "none";
+    };
+});
+document.querySelector(".find_posts_area input").addEventListener("blur", function () {
+    if (window.innerWidth <= 1060) {
+        document.querySelector(".find_posts_area").setAttribute("class", "find_posts_area"); document.getElementById("menu-button")
+        document.getElementById("menu-button").style.display = "block";
+    };
 });
 window.onresize = function () {
-    if (window.innerWidth >= 1060) {
-        document.querySelector("nav").style.display = "block";
-    } else {
-        document.querySelector("nav").style.display = "none";
-        document.getElementById("menu-button").innerHTML = '<ion-icon name="menu-outline"></ion-icon>'
-    }
-}
+    if (document.hasFocus() == false) {
+        if (window.innerWidth >= 1060) {
+            document.querySelector("nav").style.display = "block";
+        } else {
+            document.querySelector("nav").style.display = "none";
+            document.getElementById("menu-button").innerHTML = '<ion-icon name="menu-outline"></ion-icon>'
+        };
+    };
+};
