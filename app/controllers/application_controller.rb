54e232bb1d06dfa06dbe51f80d7153d58ce8661e
Lusaca <kny70dr@gmail.com>
2020-09-22 20:03:18 +0900

    リダイレクト設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :ensure_domain
+
+  def ensure_domain
+    if request.host == "the-lusaca-blog.herokuapp.com"
+      redirect_to "https://blog.lusaca.net"
+    end
+  end
 end
