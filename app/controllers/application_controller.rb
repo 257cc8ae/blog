118f2a6a51c150e68494c54791032e8cc171f1c7
Lusaca <kny70dr@gmail.com>
2020-09-24 20:54:02 +0900

    バグの修正()

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,8 @@ class ApplicationController < ActionController::Base
   before_action :ensure_domain
 
   def ensure_domain
-    if controller.controller_name != "posts" || controller.controller_name != "tags"
-      if request.host == "the-lusaca-blog.herokuapp.com"
-        redirect_to "https://blog.lusaca.net"
-      end
+    if request.host == "the-lusaca-blog.herokuapp.com"
+      redirect_to "https://blog.lusaca.net"
     end
   end
 end
