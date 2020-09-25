2efe2ba4777222427b89ece5131ab67ee8b9df46
Lusaca <kny70dr@gmail.com>
2020-09-25 21:45:29 +0900

    記事の更新ができない問題を修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,2 @@
 class ApplicationController < ActionController::Base
-  before_action :ensure_domain
-
-  def ensure_domain
-    if request.host == "the-lusaca-blog.herokuapp.com"
-      redirect_to "https://blog.lusaca.net"
-    end
-  end
 end
