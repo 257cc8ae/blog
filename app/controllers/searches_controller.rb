df9a452736a0160b8e333eff7d320e5d0dec1b4c
Lusaca <kny70dr@gmail.com>
2020-11-20 22:08:18 +0900

    仕様の変更

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,5 +1,7 @@
 class SearchesController < ApplicationController
   def index
-    @posts = Post.where(['title LIKE ?', "%#{params[:word]}%"]).or(Post.where(['content LIKE ?', "%#{params[:word]}%"])).or(Post.where(['tags LIKE ?', "%#{params[:word]}%"])).page(params[:page])
+    posts = Post.where(['title LIKE ?', "%#{params[:word]}%"]).or(Post.where(['content LIKE ?', "%#{params[:word]}%"])).or(Post.where(['tags LIKE ?', "%#{params[:word]}%"])).page(params[:page])
+    posts.each do |post|
+    render :json => posts
   end
 end
